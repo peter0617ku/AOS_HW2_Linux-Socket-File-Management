056a9007d7b1655c89461407d39ae5e9674c1988
Peter MP Ku <43951316+peter0617ku@users.noreply.github.com>
2019-12-13 02:14:33 +0800

    Add files via upload

--- src/server/file_mutex.c
+++ src/server/file_mutex.c
@@ -1,17 +1,19 @@
 #include "server.h"
 
+/*Take 1 read key if no one is writing the file*/
 void mutex_read_lock(char s[])
 {
 	for(int i=0;i<file_num;i++)
 	{
 		if(strcmp(file_list[i].file_name , s)==0)
 		{
-			while(file_list[i].mutex_write != 1);
+			while(file_list[i].mutex_write != 1);/*If someone is writing the file*/
 			file_list[i].mutex_read--;
 			break;
 		}
 	}
 }
+/*Release 1 read key*/
 void mutex_read_unlock(char s[])
 {
 	for(int i=0;i<file_num;i++)
@@ -23,20 +25,22 @@ void mutex_read_unlock(char s[])
 		}
 	}
 }
+/*Take 1 write key & 6 read key if no one is reading and writing the file*/
 void mutex_write_lock(char s[])
 {
 	for(int i=0;i<file_num;i++)
 	{
 		if(strcmp(file_list[i].file_name , s)==0)
 		{
-			while(file_list[i].mutex_read != 6);
+			while(file_list[i].mutex_read != 6);/*If someone is reading the file*/
 			file_list[i].mutex_read=0;
-			while(file_list[i].mutex_write != 1);
+			while(file_list[i].mutex_write != 1);/*If someone is writing the file*/
 			file_list[i].mutex_write--;
 			break;
 		}
 	}
 }
+/*Release 1 write key & 6 read key*/
 void mutex_write_unlock(char s[])
 {
 	for(int i=0;i<file_num;i++)
@@ -49,6 +53,7 @@ void mutex_write_unlock(char s[])
 		}
 	}
 }
+/*Check if this file exists in the file list (1: Yes, 0: No)*/
 int if_exist(char s[])
 {
 	for(int i=0;i<file_num;i++)
